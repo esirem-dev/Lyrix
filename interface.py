2d7c488c61f8c615fa8d9a91f51cf8eb82800850
Nicooow <nicolas.boujot@icloud.com>
2023-10-03 10:02:20 +0200

    check bpm on new song

--- interface.py
+++ interface.py
@@ -8,6 +8,7 @@ from PySide2.QtCore import QTimer, QObject, Signal, QThread, SIGNAL, SLOT
 import time
 
 from lyrix import Lyrix
+from utils.bpm import BPM
 
 from colorthief import ColorThief
 from urllib.request import urlopen
@@ -19,14 +20,16 @@ app.setOrganizationDomain("nicow.eu")
 app.setApplicationName("LyriX")
 # set the logo of the application
 app.setWindowIcon(QIcon("img/logo.ico"))
-    
+
 engine = QQmlApplicationEngine()
 engine.quit.connect(app.quit)
-engine.load('main.qml')
+engine.load("main.qml")
+
 
 def read_file(file_name):
-    with open(file_name, 'rb') as f:
+    with open(file_name, "rb") as f:
         return f.read()
+
 
 class WorkerToken(QObject):
     finished = Signal()
@@ -44,13 +47,14 @@ class WorkerToken(QObject):
         self.loadToken()
 
     def loadToken(self):
-        if(self.app.lyrix.token!=""):
+        if self.app.lyrix.token != "":
             return
-        if(not self.app.lyrix.loadAccessToken()):
+        if not self.app.lyrix.loadAccessToken():
             self.error.emit()
         else:
             self.timerToken.stop()
             self.finished.emit()
+
 
 class WorkerCurrentlyPlaying(QObject):
     finished = Signal()
@@ -61,7 +65,7 @@ class WorkerCurrentlyPlaying(QObject):
         super().__init__()
         self.app = app
 
-    def run(self):        
+    def run(self):
         self.timerCurrentlyPlaying = QTimer()
         self.timerCurrentlyPlaying.setInterval(5000)
         self.timerCurrentlyPlaying.timeout.connect(self.exec)
@@ -70,29 +74,77 @@ class WorkerCurrentlyPlaying(QObject):
 
     def exec(self):
         newCurrentlyPlaying = self.app.lyrix.getCurrentlyPlaying()
-        
-        if(newCurrentlyPlaying == ""):
+
+        if newCurrentlyPlaying == "":
             self.spotifyNotStarted.emit()
             return
-        
-        if(self.app.currentlyPlaying == "" or newCurrentlyPlaying["item"]["id"] != self.app.currentlyPlaying["item"]["id"]):
+
+        if (
+            self.app.currentlyPlaying == ""
+            or newCurrentlyPlaying["item"]["id"]
+            != self.app.currentlyPlaying["item"]["id"]
+        ):
             lyrics = self.app.lyrix.getLyrics(newCurrentlyPlaying["item"]["id"])
-            
-            if(lyrics == ""):
-                imgUrl = newCurrentlyPlaying["item"]["album"]["images"][0]["url"].replace("https", "http")            
+
+            if lyrics == "":
+                imgUrl = newCurrentlyPlaying["item"]["album"]["images"][0][
+                    "url"
+                ].replace("https", "http")
                 fd = urlopen(imgUrl)
                 f = io.BytesIO(fd.read())
                 color_thief = ColorThief(f)
-                backgroundColor = '#%02x%02x%02x' % color_thief.get_color(quality=1)
+                backgroundColor = "#%02x%02x%02x" % color_thief.get_color(quality=1)
                 textColor = "#eeeeee"
             else:
-                backgroundColor = "#" + format(lyrics["colors"]["background"] + (1 << 24), "x").rjust(6, "0")
-                textColor = "#" + format(lyrics["colors"]["text"] + (1 << 24), "x").rjust(6, "0")
+                backgroundColor = "#" + format(
+                    lyrics["colors"]["background"] + (1 << 24), "x"
+                ).rjust(6, "0")
+                textColor = "#" + format(
+                    lyrics["colors"]["text"] + (1 << 24), "x"
+                ).rjust(6, "0")
 
             self.newSong.emit(newCurrentlyPlaying, lyrics, backgroundColor, textColor)
-            
+
         self.app.currentlyPlaying = newCurrentlyPlaying
         self.app.lastTimeRefresh = time.time()
+
+
+class WorkerBPM(QObject):
+    newBPM = Signal([int])
+
+    def __init__(self, app):
+        super().__init__()
+        self.app = app
+        self.last_song = None
+        self.bpm = BPM()
+
+    def run(self):
+        self.timerCheckBPM = QTimer()
+        self.timerCheckBPM.setInterval(1000)
+        self.timerCheckBPM.timeout.connect(self.exec)
+        self.timerCheckBPM.start()
+        self.exec()
+
+    def exec(self):
+        song = self.app.currentlyPlaying
+        if song == "" or song is None:
+            return
+
+        try:
+            if (
+                self.last_song is None
+                or song["item"]["id"] != self.last_song["item"]["id"]
+            ):
+                self.last_song = song
+                artist = song["item"]["artists"][0]["name"]
+                title = song["item"]["name"]
+                new_bpm = self.bpm.get_bpm(artist, title)
+                self.newBPM.emit(new_bpm)
+        except Exception as e:
+            import traceback
+
+            traceback.print_exc()
+
 
 class Backend(QObject):
     clearLyrics = Signal(None)
@@ -109,7 +161,7 @@ class Backend(QObject):
         self.lastTimeRefresh = 0
         self.lyrics = {}
         self.lastIndexLine = -1
-        
+
     def start(self):
         self.updateColor.emit("#333", "#fff")
         self.updateCover.emit("")
@@ -118,7 +170,7 @@ class Backend(QObject):
         self.addLyric.emit(1, "LyriX By Nicow")
         self.selectLine.emit(0)
         self.setSyncType.emit("LINE_SYNCED")
-        
+
         self.threadToken = QThread()
         self.workerToken = WorkerToken(self)
         self.workerToken.moveToThread(self.threadToken)
@@ -149,6 +201,13 @@ class Backend(QObject):
         self.timerLineSelector.timeout.connect(self.threadLineSelector)
         self.timerLineSelector.start()
 
+        self.threadBPM = QThread()
+        self.workerBPM = WorkerBPM(self)
+        self.workerBPM.moveToThread(self.threadBPM)
+        self.threadBPM.started.connect(self.workerBPM.run)
+        self.workerBPM.newBPM.connect(self.newBPM)
+        self.threadBPM.start()
+
     def spotifyNotStarted(self):
         self.clearLyrics.emit()
         self.addLyric.emit(0, "Spotify n'est pas démarré")
@@ -160,32 +219,52 @@ class Backend(QObject):
     def tokenError(self):
         self.clearLyrics.emit()
         self.addLyric.emit(0, "Erreur")
-        self.addLyric.emit(1, "Impossible d'obtenir le token Spotify,\nvérifiez votre connexion internet")
+        self.addLyric.emit(
+            1,
+            "Impossible d'obtenir le token Spotify,\nvérifiez votre connexion internet",
+        )
         self.addLyric.emit(2, "Nouvelle tentative dans 3 secondes...")
-        self.selectLine.emit(1)       
+        self.selectLine.emit(1)
         self.setSyncType.emit("LINE_SYNCED")
-   
+
     def linkLyrix(self, link):
         self.lyrix = link
-    
+
     def newSong(self, song, lyrics, backgroundColor="#333", textColor=""):
         self.clearLyrics.emit()
         self.lyrics = lyrics
-        
-        if(self.lyrics == ""):
-            self.lyrics = {'lyrics': {'lines': []}, 'colors': {'background': -0, 'text': -0, 'highlightText': -1}}
-            self.lyrics["lyrics"]["lines"].append({'startTimeMs': '0', 'words': "Oups...", 'syllables': []})
-            self.lyrics["lyrics"]["lines"].append({'startTimeMs': '5000', 'words': "J'ai un trou de mémoire...", 'syllables': []})
-            self.lyrics["lyrics"]["lines"].append({'startTimeMs': str(60*1000*10), 'words': "Promis je vais essayer de retrouver pour la prochaine fois !", 'syllables': []})
+
+        if self.lyrics == "":
+            self.lyrics = {
+                "lyrics": {"lines": []},
+                "colors": {"background": -0, "text": -0, "highlightText": -1},
+            }
+            self.lyrics["lyrics"]["lines"].append(
+                {"startTimeMs": "0", "words": "Oups...", "syllables": []}
+            )
+            self.lyrics["lyrics"]["lines"].append(
+                {
+                    "startTimeMs": "5000",
+                    "words": "J'ai un trou de mémoire...",
+                    "syllables": [],
+                }
+            )
+            self.lyrics["lyrics"]["lines"].append(
+                {
+                    "startTimeMs": str(60 * 1000 * 10),
+                    "words": "Promis je vais essayer de retrouver pour la prochaine fois !",
+                    "syllables": [],
+                }
+            )
             self.setNoLyrics.emit(True)
         else:
             self.setNoLyrics.emit(False)
 
-        if("syncType" in self.lyrics["lyrics"]):
+        if "syncType" in self.lyrics["lyrics"]:
             self.setSyncType.emit(self.lyrics["lyrics"]["syncType"])
         else:
             self.setSyncType.emit("LINE_SYNCED")
-            
+
         i = 0
         for l in self.lyrics["lyrics"]["lines"]:
             self.addLyric.emit(i, l["words"])
@@ -193,53 +272,66 @@ class Backend(QObject):
 
         # backgroundColor = "#" + str(hex(abs(self.lyrics["colors"]["background"]))).replace("0x", "")
         imgUrl = song["item"]["album"]["images"][0]["url"].replace("https", "http")
-        
+
         self.updateColor.emit(backgroundColor, textColor)
         self.updateCover.emit(imgUrl)
-            
-    def threadCurrentlyPlaying(self):
-        pass
-            
+
+    def newBPM(self, bpm):
+        print("new bpm", bpm)
+
     def threadLineSelector(self):
-        if(self.currentlyPlaying=="" or not "lyrics" in self.lyrics):
+        if self.currentlyPlaying == "" or not "lyrics" in self.lyrics:
             return
-        
-        if("syncType" in self.lyrics["lyrics"]):
-            if(self.lyrics["lyrics"]["syncType"] == "UNSYNCED"):
-                if(self.currentlyPlaying["is_playing"]):
-                    percent = int(self.currentlyPlaying["progress_ms"] + (time.time()*1000 - self.lastTimeRefresh*1000)) * 100 / self.currentlyPlaying['item']['duration_ms']
+
+        if "syncType" in self.lyrics["lyrics"]:
+            if self.lyrics["lyrics"]["syncType"] == "UNSYNCED":
+                if self.currentlyPlaying["is_playing"]:
+                    percent = (
+                        int(
+                            self.currentlyPlaying["progress_ms"]
+                            + (time.time() * 1000 - self.lastTimeRefresh * 1000)
+                        )
+                        * 100
+                        / self.currentlyPlaying["item"]["duration_ms"]
+                    )
                 else:
-                    percent = self.currentlyPlaying["progress_ms"] * 100 / self.currentlyPlaying['item']['duration_ms']
-                index = int(len(self.lyrics["lyrics"]["lines"]) * percent/100)
-                if(self.lastIndexLine != index):
+                    percent = (
+                        self.currentlyPlaying["progress_ms"]
+                        * 100
+                        / self.currentlyPlaying["item"]["duration_ms"]
+                    )
+                index = int(len(self.lyrics["lyrics"]["lines"]) * percent / 100)
+                if self.lastIndexLine != index:
                     self.lastIndexLine = index
                     self.selectLine.emit(index)
                 return 0
-            
-        if(self.currentlyPlaying["is_playing"]):
+
+        if self.currentlyPlaying["is_playing"]:
             progress_ms = int(self.currentlyPlaying["progress_ms"]) - 700
-            timing = int(progress_ms + (time.time()*1000 - self.lastTimeRefresh*1000))
+            timing = int(
+                progress_ms + (time.time() * 1000 - self.lastTimeRefresh * 1000)
+            )
         else:
             progress_ms = int(self.currentlyPlaying["progress_ms"])
             timing = int(progress_ms)
-        
-        i = 0 
+
+        i = 0
         for l in self.lyrics["lyrics"]["lines"]:
-            if(int(l["startTimeMs"]) >= timing):
+            if int(l["startTimeMs"]) >= timing:
                 break
-            i += 1 
-        
-        if(self.lastIndexLine != i):
-            self.selectLine.emit(i-1)
+            i += 1
+
+        if self.lastIndexLine != i:
+            self.selectLine.emit(i - 1)
             self.lastIndexLine = i
+
 
 backend = Backend()
 
 lyrix = Lyrix(cookies=read_file("cookies_spotify.txt"))
-print(read_file("cookies_spotify.txt"))
 backend.linkLyrix(lyrix)
 
-engine.rootObjects()[0].setProperty('backend', backend)
+engine.rootObjects()[0].setProperty("backend", backend)
 
 backend.start()
 # sys.exit(0)
