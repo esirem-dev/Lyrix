15dc6a6512d13b4be93b7a811affb59d848541d2
Nicooow <nicolas.boujot@icloud.com>
2023-10-06 15:11:22 +0200

    fix token

--- interface.py
+++ interface.py
@@ -40,20 +40,38 @@ class WorkerToken(QObject):
         self.app = app
 
     def run(self):
+        self.app.log("WorkerToken", "Démarrage...")
         self.timerToken = QTimer()
         self.timerToken.setInterval(3000)
         self.timerToken.timeout.connect(self.loadToken)
         self.timerToken.start()
+        self.timerTokenForceRefresh = QTimer()
+        self.timerTokenForceRefresh.setInterval(1000 * 60 * 10)
+        self.timerTokenForceRefresh.timeout.connect(self.loadTokenForce)
+        self.timerTokenForceRefresh.start()
         self.loadToken()
+        self.app.log("WorkerToken", "Démarré")
 
     def loadToken(self):
+        self.app.log("WorkerToken", "Exécution...")
         if self.app.lyrix.token != "":
             return
         if not self.app.lyrix.loadAccessToken():
             self.error.emit()
+            self.app.log("WorkerToken", "Erreur lors du chargement du token")
         else:
+            self.app.log("WorkerToken", "Token chargé")
             self.timerToken.stop()
             self.finished.emit()
+
+    def loadTokenForce(self):
+        self.app.log("WorkerToken", "Exécution force...")
+        try:
+            res = self.app.lyrix.loadAccessToken()
+            self.app.log("WorkerToken", "Token chargé : " + str(res))
+        except:
+            self.app.log("WorkerToken", "Erreur lors du chargement du token")
+            pass
 
 
 class WorkerCurrentlyPlaying(QObject):
